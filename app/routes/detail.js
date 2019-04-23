a82390f13ec77ea6a4831eed0f226fa1422da721
betocantu93 <betocantu93@gmail.com>
2019-04-23 02:53:27 -0500

    use algolia for model hook

--- app/routes/detail.js
+++ app/routes/detail.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
+import {
+  Product
+} from '../models';
 
 export default class DetailRoute extends Route {
+
+  async model({
+    product_id
+  }) {
+    return new Promise(resolve => {
+      window.algolia.initIndex("products").search({
+          facetFilters: [`objectID:${product_id}`]
+        },
+        function (error, content, state) {
+          resolve(new Product(content.hits.firstObject));
+        }
+      )
+    })
+  }
 }
