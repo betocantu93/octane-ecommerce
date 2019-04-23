0f072aa47898dc15ab98b40b5631674dae1dae60
betocantu93 <betocantu93@gmail.com>
2019-04-23 02:52:35 -0500

    update product model

--- app/models/index.js
+++ app/models/index.js
@@ -1,2 +1,4 @@
 export { default as ProductInCart } from './product-in-cart';
 export { default as Product } from './product';
+export { default as Facet } from './facet';
+export { default as AlgoliaResult } from './algolia-result';
--- app/models/product.js
+++ app/models/product.js
@@ -1,20 +1,36 @@
-import { tracked } from '@glimmer/tracking';
+import {
+  tracked
+} from '@glimmer/tracking';
 
 
 export default class ProductModel {
-	@tracked price = 0;
-	@tracked name = "";
-	@tracked description = "";
-	@tracked tags = [];
-	@tracked id = 0;
+  @tracked price = 0;
+  @tracked brand = "";
+  @tracked name = "";
+  @tracked description = "";
+  @tracked tags = [];
+  @tracked artwork = [];
+  @tracked releaseYear = "";
+  @tracked objectId = "";
 
-	constructor({
-		price, name, description, tags, id
-	}){
-		this.id = id;
-		this.price = price;
-		this.name = name;
-		this.description = description;
-		this.tags = tags;
-	}
+  constructor({
+    price = 0,
+    brand = "",
+    name = "",
+    description = "",
+    releaseYear = "",
+    _tags = [],
+    artwork = ["https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png"],
+    objectID = ""
+  }) {
+    this.objectID = objectID;
+    this.price = price;
+    this.brand = brand;
+    this.name = name;
+    this.description = description;
+    this.releaseYear = releaseYear;
+    this.tags = _tags;
+    this.artwork = artwork;
+  }
+
 }
