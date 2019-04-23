263541487ab7e84d59074e0af4173ec508439447
betocantu93 <betocantu93@gmail.com>
2019-04-23 02:52:18 -0500

    facet model

---
+++ app/models/facet.js
@@ -0,0 +1,21 @@
+import { tracked } from '@glimmer/tracking';
+
+export default class Facet {
+  @tracked key = "";
+  @tracked name = "";
+  @tracked count = 0;
+
+  constructor({
+    key = "",
+    name = "",
+    count = 0
+  }) {
+    this.key = key;
+    this.name = name;
+    this.count = count;
+  }
+
+  get queryParam(){
+    return `${this.key}:${this.name}`
+  }
+}
