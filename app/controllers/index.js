499113b10f563768545c4a2e7582a6f1fee900b5
betocantu93 <betocantu93@gmail.com>
2019-04-23 02:49:30 -0500

    add queryParams to index controller and route

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,51 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
+import {
+  inject as service
+} from '@ember/service';
+import {
+  action
+} from '@ember/object';
 
 export default class IndexController extends Controller {
-	@service cartManager;
+  @service cartManager;
+  @service listing;
+
+  queryParams = ['query', 'facets', 'page', 'pageSize'];
+  query = "";
+  facets = [];
+  page = 1;
+  pageSize = 20;
+
+  @action
+  toggleFacet(facet) {
+    let found = this.facets.find(f => f === facet.queryParam);
+    if (found) {
+      this.facets.removeObject(facet.queryParam);
+    } else {
+      this.facets.addObject(facet.queryParam)
+    }
+
+    this.set('page', 1);
+
+  }
+
+  @action
+  reset() {
+    this.setProperties({
+      query: '',
+      facets: [],
+      page: 1,
+      pageSize: 20
+    })
+  }
+
+  @action
+  back(){
+    this.set('page', this.page - 1);
+  }
+
+  @action
+  forward(){
+    this.set('page', this.page + 1);
+  }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,28 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import {
+  inject as service
+} from '@ember/service';
 
 export default class IndexRoute extends Route {
-	@service cartManager;
+  @service cartManager;
+  @service listing;
 
-	afterModel(){
-		super.afterModel(...arguments);
-		console.log(this.cartManager);
-	}
+  queryParams = {
+    query: {
+      refreshModel: true
+    },
+    facets: {
+      refreshModel: true
+    },
+    page: {
+      refreshModel: true
+    },
+    pageSize: {
+      refreshModel: true
+    }
+  }
+
+  model({ query, facets, page, pageSize }) {
+    return this.listing.search({ query, facets, page, pageSize })
+  }
 }
