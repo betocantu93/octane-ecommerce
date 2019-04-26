b26b54aefb2a8c22a310b77e3dfbcf66302bbe68
betocantu93 <betocantu93@gmail.com>
2019-04-25 19:06:51 -0500

    implement basic paypal and sending emails

---
+++ app/components/paypal-button.js
@@ -0,0 +1,67 @@
+import Component from '@glimmer/component';
+import {
+  inject as service
+} from '@ember/service';
+import {
+  action
+} from '@ember/object';
+import {
+  tracked
+} from '@glimmer/tracking';
+import {
+  scheduleOnce
+} from '@ember/runloop';
+
+export default class PaypalButtonComponent extends Component {
+  @service cartManager;
+  @tracked email = ""
+  @tracked name = ""
+  @tracked products = ""
+  @tracked total = ""
+
+  @action
+  didInsertPaypal(ele) {
+
+		if(window.paypal) {
+			this.renderPaypal(ele);
+		} else {
+			setTimeout(() => {
+				this.renderPaypal(ele);
+			}, 2500)
+		}
+    
+
+  }
+
+  renderPaypal(ele) {
+    paypal.Buttons.call(this, {
+      createOrder: (data, actions) => {
+        return actions.order.create(this.cartManager.toPayPal);
+      },
+      onApprove: (data, actions) => {
+        return actions.order.capture().then((details) => {
+          this.name = details.payer.name.given_name
+          this.email = details.payer.email_address
+          this.total = details.purchase_units[0].amount.value
+          this.products = `<ul>${details.purchase_units[0].items.map(({sku, name, quantity, unit_amount}) => {
+				return ` < li > Product $ {
+            sku
+          } - $ {
+            name
+          }
+          X $ {
+            quantity
+          } = $$ {
+            quantity * unit_amount.value
+          } < /li>`
+        }).join(', ')
+      } < /ul>`
+
+      scheduleOnce('afterRender', this, () => {
+        document.formspree.submit()
+      })
+    });
+  }
+}).render(ele);
+}
+}
