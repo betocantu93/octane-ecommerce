751a1118588b932b80b2c10748290f0f6ce3202d
betocantu93 <betocantu93@gmail.com>
2019-04-25 19:08:47 -0500

    add quantity input in detail

--- app/controllers/detail.js
+++ app/controllers/detail.js
@@ -1,8 +1,9 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-
+import { tracked } from '@glimmer/tracking';
 
 export default class DetailController extends Controller {
+	@tracked quantity = 1;
 
 	@service cartManager;
 	
