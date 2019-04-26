fb838715fa786493c04b5bfe1c66c71b76c35d14
betocantu93 <betocantu93@gmail.com>
2019-04-26 03:41:44 -0500

    fix accesability and SEO, add ember-web-app for manifest

---
+++ config/manifest.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/zonkyio/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "Ember Octane Ecommerce",
+    short_name: "Ember Octane Ecommerce",
+    description: "Ember Octane Ecommerce",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000",
+    icons: [
+    ],
+    ms: {
+      tileColor: '#000000'
+    }
+  };
+}
