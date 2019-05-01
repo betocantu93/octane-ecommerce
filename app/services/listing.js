7a8033ae11ba861e148b9ddc0d692d645ecb81b1
betocantu93 <betocantu93@gmail.com>
2019-05-01 02:42:40 -0500

    formatting, getFacets and use algolia now

--- app/services/listing.js
+++ app/services/listing.js
@@ -4,18 +4,18 @@ import {
 } from '@glimmer/tracking';
 import {
   Product,
-	Facet,
-	AlgoliaResult
+  Facet,
+  AlgoliaResult
 } from '../models';
 import {
   action
 } from '@ember/object';
 
-
 export default class ListingService extends Service {
   @tracked currentIndex = this.ALGOLIA_INDEXES[0];
   @tracked lastResult = new AlgoliaResult({});
-	@tracked facets = [];
+  @tracked facets = [];
+  @tracked algolia = null;
 
   ALGOLIA_INDEXES = [
     "products"
@@ -26,37 +26,50 @@ export default class ListingService extends Service {
     this.currentIndex = this.ALGOLIA_INDEXES.find(ele => ele === index) || ALGOLIA_INDEXES[0];
   }
 
-  constructor() {
-    super(...arguments);
-    window.algolia.initIndex(this.currentIndex).search(
-      {
-        facets: ['*']
-      },
-      function (error, content) {
-        let facets = [];
-        Object.keys(content.facets).forEach(facetKey => {
-          let facet = content.facets[facetKey];
-          Object.keys(facet).forEach(key => {
-            facets.addObject(new Facet({key: facetKey, name: key, count: facet[key]}))
-          })	
-        })
-        this.facets = facets;
-      }.bind(this)
-    );
+  @action
+  async getFacets() {
+
+    await new Promise(resolve => {
+      this.algolia.initIndex(this.currentIndex).search({
+          facets: ['*']
+        },
+        function (error, content) {
+          let facets = [];
+          Object.keys(content.facets).forEach(facetKey => {
+            let facet = content.facets[facetKey];
+            Object.keys(facet).forEach(key => {
+              facets.addObject(new Facet({
+                key: facetKey,
+                name: key,
+                count: facet[key]
+              }))
+            })
+          })
+          this.facets = facets;
+          resolve(facets);
+        }.bind(this)
+      );
+    })
+
   }
 
   @action
-  async search({ query, facets: facetsFilters, page, pageSize }) {
-    
+  async search({
+    query,
+    facets: facetsFilters,
+    page,
+    pageSize
+  }) {
+
     let parsedFilters = Array.isArray(facetsFilters) ? facetsFilters : JSON.parse(facetsFilters);
 
     let brandFilters = parsedFilters.filter(facet => facet.split(':')[0] === "brand").map(facet => {
-      let [ type, name ] = facet.split(':');
+      let [type, name] = facet.split(':');
       return `${type}:"${name}"`
     }).join(' OR ')
 
     let tagFilters = parsedFilters.filter(facet => facet.split(':')[0] === "_tags").map(facet => {
-      let [ type, name ] = facet.split(':');
+      let [type, name] = facet.split(':');
       return `${type}:"${name}"`
     }).join(' OR ')
 
@@ -64,9 +77,9 @@ export default class ListingService extends Service {
     tagFilters = tagFilters.length ? `(${tagFilters})` : tagFilters;
     let filters = "";
 
-    if(brandFilters.length && tagFilters.length) {
+    if (brandFilters.length && tagFilters.length) {
       filters = `${brandFilters} AND ${tagFilters}`;
-    } else if(brandFilters.length) {
+    } else if (brandFilters.length) {
       filters = brandFilters;
     } else {
       filters = tagFilters;
@@ -77,23 +90,24 @@ export default class ListingService extends Service {
       facets: ['*'],
       page: page - 1,
       hitsPerPage: pageSize,
-			filters
+      filters
     };
 
     let results = await new Promise(resolve => {
-      window.algolia.initIndex(this.currentIndex).search(
+      this.algolia.initIndex(this.currentIndex).search(
         algoliaFilters,
         function (error, content) {
           this.lastResult.total = content.nbHits;
           this.lastResult.totalPages = content.nbPages;
           let results = content.hits.map(hit => new Product(hit));
-					this.lastResult.hits = results;
-					
+          this.lastResult.hits = results;
+
           resolve(results);
         }.bind(this)
       );
     })
     return results;
-	}
-	
+
+  }
+
 }
