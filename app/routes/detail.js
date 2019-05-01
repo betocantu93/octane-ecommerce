d285bde29c8335d2441251149aaaa2dc07dd4011
betocantu93 <betocantu93@gmail.com>
2019-05-01 02:38:46 -0500

    dont use firstObject, its not FastBoot safe

--- app/routes/detail.js
+++ app/routes/detail.js
@@ -2,24 +2,28 @@ import Route from '@ember/routing/route';
 import {
   Product
 } from '../models';
-
+import {
+  inject as service
+} from '@ember/service';
 export default class DetailRoute extends Route {
+  @service listing;
 
   async model({
     product_id
   }) {
     return new Promise(resolve => {
-      window.algolia.initIndex("products").search({
+      this.listing.algolia.initIndex("products").search({
           facetFilters: [`objectID:${product_id}`]
         },
         function (error, content) {
-          resolve(new Product(content.hits.firstObject));
+          resolve(new Product(content.hits.pop()));
         }
       )
     })
+
   }
 
-  setupController(controller){
+  setupController(controller) {
     super.setupController(...arguments);
     controller.quantity = 1;
   }
