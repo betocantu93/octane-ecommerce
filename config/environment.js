e3a3d1f99514cb78c630fb708a83cdaeba098bfb
betocantu93 <betocantu93@gmail.com>
2019-05-01 02:42:07 -0500

    fastboot whitelist

--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,9 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    fastboot: {
+      hostWhitelist: ['http://octane-ecommerce.surge.sh', /^localhost:\d+$/]
     }
   };
 
