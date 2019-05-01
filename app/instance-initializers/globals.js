23779449f4efb09115fb9b4bdc43d106683b5fd7
betocantu93 <betocantu93@gmail.com>
2019-05-01 02:26:56 -0500

    remove algoliasearch from window

--- app/instance-initializers/globals.js
+++ app/instance-initializers/globals.js
@@ -1,9 +1,6 @@
 import ENV from '../config/environment';
-import algoliasearch from 'algoliasearch';
 
 export function initialize(appInstance) {
-
-  window.algolia = algoliasearch('FEDPQMZMUP', 'd91401df3b347bbc2bb83a1c8142bab4');
 
   if (ENV.environment === 'development') {
     let cartManager = appInstance.lookup('service:cart-manager');
