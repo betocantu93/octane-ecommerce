ef89d392cb1a106d27d8ebc2c087e9a4c8ed1918
betocantu93 <betocantu93@gmail.com>
2019-06-06 15:59:03 -0500

    handle 405 error

--- app/routes/make-products.js
+++ app/routes/make-products.js
@@ -8,7 +8,14 @@ export default class MakeProductsRoute extends Route {
   }
   async request(endpoint) {
     let api_key = `74063a8076247e9d94610f1001a80cda`
-    return await (await fetch(`http://ws.audioscrobbler.com/2.0/?method=${endpoint}&api_key=${api_key}&format=json`)).json()
+    try {
+      return await (await fetch(`http://ws.audioscrobbler.com/2.0/?method=${endpoint}&api_key=${api_key}&format=json`)).json()
+    } catch(e) {
+      if(endpoint.includes("gettoptags")) {
+        return { toptags: { tag: [] } }
+      }
+      return { topalbums: { album: [] } }
+    }
   }
 
 
@@ -24,31 +31,37 @@ export default class MakeProductsRoute extends Route {
     return this.request(`artist.gettoptags&mbid=${mbid}`)
   }
   async getRequests() {
-    let artistsObj = await this.request('chart.gettopartists')
-    let albumsAndTags = await Promise.all(artistsObj.artists.artist.map(async artist => {
-      return {
-        tags: await this.getTags(artist),
-        albums: await this.getAlbums(artist)
-      }
-    }));
-    let products = []
-    albumsAndTags.filter(albumAndTag => !albumAndTag.albums.error).forEach(({
-      albums,
-      tags
-    }) => {
+    try {
+      let artistsObj = await this.request('chart.gettopartists')
+      let albumsAndTags = await Promise.all(artistsObj.artists.artist.map(async artist => {
+        return {
+          tags: await this.getTags(artist) || { toptags: { tag: [] } },
+          albums: await this.getAlbums(artist) || { topalbums: { album: [] } }
+        }
+      }));
 
-      products.pushObjects(
-        albums.topalbums.album.map(album => {
-          return {
-            price: `${Math.floor(Math.random() * (50 * 100 - 1 * 100) + 1 * 100) / (1*100)}`,
-            brand: albums.topalbums[`@attr`].artist,
-            name: album.name,
-            artwork: [album.image.lastObject[`#text`]],
-            _tags: tags.toptags.tag.slice(0, 2).mapBy('name')
-          }
-        })
-      );
-    })
-    console.log(JSON.stringify(products)) // eslint-disable-line no-console
+      let products = []
+      albumsAndTags.filter(albumAndTag => !albumAndTag.albums.error).forEach(({
+        albums,
+        tags
+      }) => {
+
+        products.pushObjects(
+          albums.topalbums.album.map(album => {
+            return {
+              price: `${Math.floor(Math.random() * (50 * 100 - 1 * 100) + 1 * 100) / (1*100)}`,
+              brand: albums.topalbums[`@attr`].artist,
+              name: album.name,
+              artwork: [album.image.lastObject[`#text`]],
+              _tags: tags.toptags.tag.slice(0, 2).mapBy('name')
+            }
+          })
+        );
+      })
+      console.log(JSON.stringify(products)) // eslint-disable-line no-console
+    } catch (e) {
+      console.log(e);
+    }
+    
   }
 }
