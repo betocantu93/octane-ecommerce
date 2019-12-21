c3f01eae30d406568f8d3a0ccaa0f12f5c6d237b
betocantu93 <betocantu93@gmail.com>
2019-12-21 10:04:31 -0600

    update to 3.15

--- app/router.js
+++ app/router.js
@@ -1,15 +1,13 @@
 import EmberRouter from "@ember/routing/router";
 import config from "./config/environment";
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('detail', { path: 'detail/:product_id'});
   this.route('cart');
   this.route('make-products');
 });
-
-export default Router;
