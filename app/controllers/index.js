c851c27a17d64d9b6c0f5cee181dfc767c513ac7
betocantu93 <betocantu93@gmail.com>
2019-12-21 12:56:11 -0600

    for better SEO, lets just create LinkTo's

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -54,11 +54,6 @@ export default class IndexController extends Controller {
   }
 
   @action
-  goToProduct(product) {
-    this.transitionToRoute('detail', product.objectID);
-  }
-
-  @action
   back(){
     this.set('page', this.page - 1);
   }
